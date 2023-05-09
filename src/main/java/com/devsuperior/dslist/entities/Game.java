ed64692d23582055012771a164e660ec5b3f4c4b
Igor Wiepieski <iw.programacao@gmail.com>
2023-05-09 16:08:58 -0300

    properties, entidade Games e ORM

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author igorw
+ */
+@Entity
+@Table(name = "games")
+//LOMBOK - Substitui códigos repetitivos obrigatórios para Hibernate
+//@Getter, @Setter e @ToString podem ser substituidos pelo @Data
+//@Getter
+//@Setter
+//@ToString
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // Padrão de Builder
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    
+    private String genre;
+    
+    private String platforms;
+    
+    private Double score;
+    
+    @Column(name = "img_url")
+    private String imgUrl;
+    
+    @Column(name = "short_description")
+    private String shortDescription;
+    
+    @Column(name = "long_description")
+    private String longDescription;
+
+}
